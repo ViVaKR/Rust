fb7231db6206bb25106cda4efd8c201b6a3b9e4a
ViVaKR <admin@vivabj.com>
2023-04-21 22:53:37 +0900

    Update Array & loop, while loop, for loop

--- viv_demo/src/array_.rs
+++ viv_demo/src/array_.rs
@@ -1,12 +1,50 @@
 #![allow(unused)]
 
-use std::io;
 use rand::Rng;
-use std::io:: {Write, BufReader, BufRead, ErrorKind};
-use std::fs::File;
 use std::cmp::Ordering;
+use std::collections::btree_map::Values;
+use std::fs::File;
+use std::io;
+use std::io::{BufRead, BufReader, ErrorKind, Write};
 
 pub fn array_() {
     println!();
     println!("***** Array *****");
+
+    let arr_1: [i32; 9] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    println!("1st: {}", arr_1[0]);
+    println!("Length : {}", arr_1.len());
+    let mut loop_idx: usize = 0;
+
+    println!("\n***** loop *****");
+    loop {
+        if arr_1[loop_idx] % 2 == 0 {
+            loop_idx += 1;
+            continue;
+        }
+
+        if arr_1[loop_idx] == 9 {
+            break;
+        }
+
+        println!("Val: {}", arr_1[loop_idx]);
+        loop_idx += 1;
+    }
+
+    println!("\n***** While Loop *****");
+    loop_idx = 0;
+    while loop_idx < arr_1.len() {
+         println!("Arr : {}", arr_1[loop_idx]);
+
+         loop_idx += 1;
+    }
+
+    println!("\nfor loop");
+    for val in arr_1 {
+        println!("Var : {}", val);
+    }
+
+
+
 }
--- viv_demo/src/match_.rs
+++ viv_demo/src/match_.rs
@@ -1,34 +1,31 @@
 #![allow(unused)]
 
-use std::io;
 use rand::Rng;
-use std::io:: {Write, BufReader, BufRead, ErrorKind};
-use std::fs::File;
 use std::cmp::Ordering;
-
+use std::fs::File;
+use std::io;
+use std::io::{BufRead, BufReader, ErrorKind, Write};
 
 pub fn match_() {
     println!();
     println!("***** Match *****");
     let age: i32 = 48;
-    
+
     match age {
         1..=18 => println!("Young man"),
         19 => println!("Good"),
         20 => println!("Nice"),
         21 | 50 => println!("Strong man"),
         65..=i32::MAX => println!("Hi there"),
-        _=> println!("Unknown")
+        _ => println!("Unknown"),
     };
 
     let voting_age = 18;
 
     match age.cmp(&voting_age) {
-        Ordering:: Less => println!("Can't Vote"),
-        Ordering:: Greater => println!("Can Vote"),
-        Ordering:: Equal => println!("You gained the rigth to vote"),
-        _=> println!("Hello World!")
+        Ordering::Less => println!("Can't Vote"),
+        Ordering::Greater => println!("Can Vote"),
+        Ordering::Equal => println!("You gained the rigth to vote"),
+        _ => println!("Hello World!"),
     }
 }
-
-
